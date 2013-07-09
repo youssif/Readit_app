582598ca1e4d2fb2ed7ab5933ec210508530bfe1
Youssif <abdulyoussif@gmail.com>
2013-07-09 08:38:24 -0700

    links can be uploaded

--- readit/db/schema.rb
+++ readit/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130708173130) do
+ActiveRecord::Schema.define(:version => 20130709152908) do
+
+  create_table "links", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "url"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.string   "title"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email"
