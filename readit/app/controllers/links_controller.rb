001ea8d7c2ba12e8a07f85f9d768a4012000d55f
Youssif <abdulyoussif@gmail.com>
2013-07-09 10:14:37 -0700

    finished with being able to post new link and title

---
+++ readit/app/controllers/links_controller.rb
@@ -0,0 +1,19 @@
+class LinksController < ApplicationController
+	def show
+		@link = Link.find(params[:id])
+
+	end
+	
+	def new
+		@link = Link.new
+	end
+	
+	def create
+		@link = Link.new(params[:link])
+		if @link.save 
+			redirect_to @link
+		else
+			render action: 'new'
+		end
+	end
+end
--- readit/config/routes.rb
+++ readit/config/routes.rb
@@ -4,6 +4,8 @@ Readit::Application.routes.draw do
 
   resources :pages
 
+  resources :links
+
   root to: "pages#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
